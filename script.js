b1dc65e22f86eb801057ea0314a588999b8f6860
DianaMDG <dianamdgoncalves+dei@gmail.com>
2025-10-29 08:14:34 +0000

    Create images table in Script.js
    and alter 1 instance of data to match variable names

--- script.js
+++ script.js
@@ -17,19 +17,45 @@ fetch('plants.json')
         </div>
       `;
       
-      const img = document.createElement('img');
+      /*const img = document.createElement('img');
       img.src = plant.imageWhole;
       img.alt = plant.nameCommon;
+      */
 
       const photos = document.createElement('div');
-      details.className = 'plant-photos';        /*Classe details*/ 
-      details.innerHTML = `
-        <h2>${plant.nameCommon}</h2>
-        <p><strong>Family:</strong> ${plant.nameScientific}</p>
-        <p><strong>Family:</strong> ${plant.family}</p>
-        <p><strong>Description:</strong> ${plant.description}</p>
-        <p><strong>Habitat:</strong> ${plant.habitat}</p>
-        <p><strong>Uses:</strong> ${plant.uses}</p>
+      photos.className = 'plant-photos';        /*Class details*/ 
+
+      // choose main image and up to 4 more images
+      const wholeSpecimen = plant.imageWhole || (plant.photos && plant.photos[0]) || '';
+      const photoList = (plant.photos).slice(1, 5);
+      while (photoList.length < 4) others.push(''); // ensure 4 items
+
+      const photoLabelsList = (plant.photoLabels).slice(1, 5);
+      while (photoLabelsList.length < 4) others.push(''); // ensure 4 items
+
+
+      photos.innerHTML = `
+        <table style="width:100%">
+          <tr>
+            <td rowspan="2" style=" height:108mm; width:auto; background-color: green">
+              <img src="${wholeSpecimen}" alt="${plant.nameCommon}" style="width:100%; height:100%; object-fit:cover; display:block" />
+            </td>
+            <td style="width: 48mm; height:48mm; background-color: green">
+              <img src="${photoList[0]}" alt="${photoLabelsList[0]} photo2" style="width:100%; height:100%; object-fit:cover; display:block" />
+            </td>
+            <td style="width: 48mm; height: 48mm; background-color: green">
+              <img src="${photoList[1]}" alt="${photoLabelsList[1]} photo3" style="width:100%; height:100%; object-fit:cover; display:block" />
+            </td>
+          </tr>
+          <tr>
+            <td style="width: 48mm; height:48mm; background-color: green">
+              <img src="${photoList[2]}" alt="${photoLabelsList[2]} photo4" style="width:100%; height:100%; object-fit:cover; display:block" />
+            </td>
+            <td style="width: 48mm; height:48mm; background-color: green">
+              <img src="${photoList[3]}" alt="${photoLabelsList[3]} photo5" style="width:100%; height:100%; object-fit:cover; display:block" />
+            </td>
+          </tr>
+        </table>
       `;
 
       sheet.appendChild(firstLine);
